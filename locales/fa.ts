e6b4c44e803530ec5bfd97c0098d1280bfcafa22
AmirMahmood <amma4ww@gmail.com>
2022-12-27 13:35:01 +0330

    Update Translation and version

--- locales/fa.ts
+++ locales/fa.ts
@@ -6,13 +6,46 @@
     <message>
         <location filename="../ui/dialog_about.ui" line="14"/>
         <source>About</source>
-        <translation type="obsolete">درباره</translation>
+        <translation>درباره</translation>
+    </message>
+    <message>
+        <location filename="../ui/dialog_about.ui" line="26"/>
+        <source>EasyPrint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/dialog_about.ui" line="33"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;meta charset=&quot;utf-8&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+hr { height: 1px; border-width: 0; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Segoe UI&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Icons by &lt;a href=&quot;https://icons8.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;icons8&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FavFontDialog</name>
+    <message>
+        <location filename="../ui/dialog_favfont.ui" line="14"/>
+        <source>Favorite Fonts</source>
+        <translation>فونت های موردعلاقه</translation>
+    </message>
+    <message>
+        <location filename="../ui/dialog_favfont.ui" line="23"/>
+        <source>Add your favorite fonts in line separated format</source>
+        <translation>نام فونت های موردعلاقه خود را در خط های مجزا بنویسید</translation>
+    </message>
+    <message>
+        <location filename="../ui/dialog_favfont.ui" line="35"/>
+        <source>Add</source>
+        <translation>افزودن</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../ui/window_main.ui" line="475"/>
+        <location filename="../ui/window_main.ui" line="500"/>
         <source>A4</source>
         <translation></translation>
     </message>
@@ -87,27 +120,27 @@
         <translation>برش</translation>
     </message>
     <message>
-        <location filename="../ui/window_main.ui" line="433"/>
+        <location filename="../ui/window_main.ui" line="451"/>
         <source>Pages</source>
         <translation>صفحه</translation>
     </message>
     <message>
-        <location filename="../ui/window_main.ui" line="442"/>
+        <location filename="../ui/window_main.ui" line="460"/>
         <source>File</source>
         <translation>فایل</translation>
     </message>
     <message>
-        <location filename="../ui/window_main.ui" line="464"/>
+        <location filename="../ui/window_main.ui" line="489"/>
         <source>Save</source>
         <translation>ذخیره</translation>
     </message>
     <message>
-        <location filename="../ui/window_main.ui" line="483"/>
+        <location filename="../ui/window_main.ui" line="508"/>
         <source>A5</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../ui/window_main.ui" line="491"/>
+        <location filename="../ui/window_main.ui" line="516"/>
         <source>Show Rulers</source>
         <translation>خط کش</translation>
     </message>
@@ -117,12 +150,12 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/window_main.ui" line="448"/>
+        <location filename="../ui/window_main.ui" line="466"/>
         <source>Help</source>
         <translation>راهنما</translation>
     </message>
     <message>
-        <location filename="../ui/window_main.ui" line="496"/>
+        <location filename="../ui/window_main.ui" line="521"/>
         <source>About</source>
         <translation>درباره</translation>
     </message>
@@ -132,7 +165,7 @@
         <translation>کپی</translation>
     </message>
     <message>
-        <location filename="../ui/window_main.ui" line="454"/>
+        <location filename="../ui/window_main.ui" line="472"/>
         <source>lang</source>
         <translation type="unfinished"></translation>
     </message>
@@ -142,14 +175,29 @@
         <translation>افزودن متن</translation>
     </message>
     <message>
-        <location filename="../ui/window_main.ui" line="397"/>
+        <location filename="../ui/window_main.ui" line="415"/>
         <source>B</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/window_main.ui" line="407"/>
+        <location filename="../ui/window_main.ui" line="425"/>
         <source>Color</source>
         <translation>رنگ</translation>
+    </message>
+    <message>
+        <location filename="../ui/window_main.ui" line="383"/>
+        <source>*</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/window_main.ui" line="477"/>
+        <source>Tools</source>
+        <translation>ابزار</translation>
+    </message>
+    <message>
+        <location filename="../ui/window_main.ui" line="526"/>
+        <source>Favorite Fonts</source>
+        <translation>فونت های موردعلاقه</translation>
     </message>
 </context>
 </TS>
